f34866a5d3ea8e72ef2f8ba25f00482afdddfdfb
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-10-05 21:42:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ function palindromeTest(){
     for(let i = 0; i < word.length; i++){
         if(word[i] === word[word.length - 1 - i]){
             console.log("Its a palindrome!")
+            alert("Its a palindrome!")
         }else if(word[i] !== word[word.length - 1 - i]){
             console.log('Its not a palindrome')
+            alert('Its not a palindrome')
         }
  }
 }
