292a55c7dff27079837d0622709af2fb9de21fbb
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-10-16 11:17:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
+//get input value
 let word = document.getElementById('input').value;
 const btn = document.querySelector('#btn')
 
 
 //compare input with reverse input
-function palindromeTest(){
-    // word.split('')
-    for(let i = 0; i < word.length; i++){
-        if(word[i] === word[word.length - 1 - i]){
-            console.log("Its a palindrome!")
-            alert("Its a palindrome!")
-        }else if(word[i] !== word[word.length - 1 - i]){
-            console.log('Its not a palindrome')
-            alert('Its not a palindrome')
-        }
- }
-}
+// function palindromeTest(){
+//     // word.split('')
+//     for(let i = 0; i < word.length; i++){
+//         if(word[i] === word[word.length - 1 - i]){
+//             console.log("Its a palindrome!")
+//         }else if(word[i] !== word[word.length - 1 - i]){
+//             console.log('Its not a palindrome')
+//         }
+//  }
+// }
 
-btn.addEventListener('click', palindromeTest)
+// btn.addEventListener('click', palindromeTest)
+
+function testPalnidrome(){
+    let reversedWord = word.split("").reverse().join("")
+    if(word === reversedWord){
+        console.log("yes")
+    }else{
+        console.log("nope")
+    }
+}
+btn.addEventListener("click", testPalnidrome)
